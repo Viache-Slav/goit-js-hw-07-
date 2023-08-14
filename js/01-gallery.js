f8d7640310bfff3cb070bb98c190c6231fc75ba7
Viache-Slav <slavikfedko@gmail.com>
2023-08-14 23:35:39 +0200

    task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEll = document.querySelector('.gallery');
+for (let img of galleryItems) {
+    const listEl = `<li class="gallery__item">
+    <a class="gallery__link" href=${img.original}>
+    <img
+        class="gallery__image"
+        src=${img.preview}
+        data-source=${img.original}
+        alt=${img.description}
+    />
+    </a>
+    </li>`;
+    galleryEll.insertAdjacentHTML("beforeend", listEl);
+}
+
+galleryEll.addEventListener("click", (event) => {
+    event.preventDefault();
+    const view = basicLightbox.create(`<img src="${event.target.dataset.source}" width="900" height="700">`);
+    view.show();
+});
