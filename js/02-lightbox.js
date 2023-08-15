f96aa312d807a8f56127ebc909bd86d97b7d5d9f
Viache-Slav <slavikfedko@gmail.com>
2023-08-15 16:26:28 +0200

    task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEll = document.querySelector('.gallery')
+const listEl = []
 
-console.log(galleryItems);
+galleryItems.forEach(element => {
+    const galleryLink = document.createElement('a')
+    galleryLink.className = 'gallery__item'
+    galleryLink.href = element.original
+
+    const galleryImage = document.createElement('img')
+    galleryImage.src = element.preview
+    galleryImage.className ='gallery__image'
+    galleryImage.setAttribute('title', element.description)
+    galleryImage.alt = element.description
+
+    galleryLink.append(galleryImage)
+    listEl.push(galleryLink)
+})
+
+galleryEll.append(...listEl)
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+})
+
